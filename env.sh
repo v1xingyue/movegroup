c7152c2e09aeb04ebdd1eefa553f3f7b50e3bce6
v1xingyue <qixingyue@126.com>
2023-01-10 11:09:35 +0800

    修改部分环境配置

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-export PATH="$PATH:/Users/xingyue/Desktop/starcoin-artifacts/:`pwd`/tools:"
+export PATH="$PATH:`pwd`/starcoin-artifacts/:`pwd`/tools:"
 export PS1='starcoin-dev $(pwd) $ '
 
-alias starconsole="starcoin -c /Users/xingyue/.starcoin/dev/starcoin.ipc console"
+alias starconsole="starcoin -c ${HOME}.starcoin/dev/starcoin.ipc console"
--- readme.md
+++ readme.md
@@ -12,3 +12,8 @@
 1. 使用 vscode 打开，安装 fabiospampinato.vscode-terminals 直接启动几个关联的terminal
 2. 终端会运行对应的env.sh 设置对应的环境变量
 3. tools 中封装了几个对应的脚本
+
+环境需求:
+
+1. nodejs nvm yarn
+2. 下载好 starcoin 二进制，修改 env.sh 的 PATH 包含进去对应的路径
